87d6bcca03aa7c4d3be31948fde2b39fffdebd46
sharkliao1122 <115081040+sharkliao1122@users.noreply.github.com>
2024-04-02 11:51:02 +0000

    20240402

--- 20240402/天氣.py
+++ 20240402/天氣.py
@@ -1,16 +1,19 @@
-import requests
-import xml.etree.ElementTree as ET
 import json
 import pandas as pd
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import os
+import requests
+
 
 import feedparser
 
 # url = https://www.cwa.gov.tw/rss/forecast/36_01.xml
 # 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, to 22
+
+county_list = []
+
 
 for num in range(1, 23):
     #string format with prefix 0 if num < 10
@@ -20,17 +23,25 @@ for num in range(1, 23):
     response = requests.get(url)
     #parse rss feed
     feed = feedparser.parse(response.content)
-    #print all titles
+
+    tempdict = {}
+
     for entry in feed.entries:
-        #print(entry.title)
-        #print(entry.title)
-        print(entry.title[1:3])
-        #print(entry.title[21:24])
+        # entry.title includes '溫度'
+        if '溫度' in entry.title:
+        # 資料的格式 如下:
+        # 金門縣04/02 今晚明晨 晴時多雲 溫度: 22 ~ 24 降雨機率: 10% (04/02 17:00發布)
+            print(entry.title)
+        #取出縣市名稱(前三個字)
+            tempdict['county'] = entry.title[:3]
 
+        #取出溫度的部分 使用空格切割後 取出 -7 與 -5 的部分
+            tempdict['min'] = entry.title.split(' ')[-7]
+            tempdict['max'] = entry.title.split(' ')[-5]
+            print(tempdict['county'], tempdict['min'], tempdict['max'])
+    
+            county_list.append(tempdict)
+        print("=======================================")
 
-        
-        
-        
-
-    print("=======================================")
-   
+df_weather = pd.DataFrame(county_list)
+print(df_weather)

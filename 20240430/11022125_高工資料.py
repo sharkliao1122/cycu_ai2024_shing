0128a7be373cb4260e9b1c684eeb09467d493bab
Shing1122 <160597889+Shing1122@users.noreply.github.com>
2024-04-30 21:34:22 +0800

    20240430

--- 20240430/11022125_高工資料.py
+++ 20240430/11022125_高工資料.py
@@ -18,6 +18,7 @@ for i in range(0, 24):
         # 將處理後的資料寫存入 C:\Users\User\OneDrive\桌面\AI與土木應用\highway
         df.to_csv(f"C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\highway\\{i:02d}{j:02d}.csv", index=False)
         print(f"{i:02d}{j:02d}.csv 下載並處理成功")
+
 # 設定目錄路徑c:\Users\User\Desktop\DATA
 directory = "C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\highway"
 
@@ -34,5 +35,43 @@ if dfs:
 
     # 將合併後的 DataFrame 寫入到一個新的 CSV 檔案並存於此位置C:\Users\User\OneDrive\桌面\AI與土木應用\GitHub\cycu_ai2024_shing\20240430\M03A
     df.to_csv(r"C:\Users\User\OneDrive\桌面\AI與土木應用\GitHub\cycu_ai2024_shing\20240430\M03A\20240429.csv", index=False)
+    print("所有 CSV 檔案已成功合併並存儲在 C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\GitHub\\cycu_ai2024_shing\\20240430\\M03A\\HW03A.csv")     
+else:
+    print("在指定的目錄下沒有找到任何 CSV 檔案。")
+
+
+
+# 處理M04A
+# 設定 CSV 檔案的 URL
+for i in range(0, 24):
+    for j in range(0, 60, 5):
+        # 設定 URL
+        url = f"https://tisvcloud.freeway.gov.tw/history/TDCS/M04A/20240429/{i:02d}/TDCS_M04A_20240429_{i:02d}{j:02d}00.csv"
+        # 下載檔案
+        response = requests.get(url)
+        # 讀取 CSV 檔案，並設定欄位名稱
+        df = pd.read_csv(io.StringIO(response.content.decode('utf-8')), names=['時間', '上游', '下游', '車種', '車速', '車流量'])
+        # 將處理後的資料寫存入 C:\Users\User\OneDrive\桌面\AI與土木應用\highway2
+        df.to_csv(f"C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\highway2\\{i:02d}{j:02d}.csv", index=False)
+        print(f"{i:02d}{j:02d}.csv 下載並處理成功")
+
+
+# 設定目錄路徑C:\Users\User\OneDrive\桌面\AI與土木應用\highway2
+directory = "C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\highway2"   
+
+# 獲取目錄下的所有檔案名稱
+filenames = os.listdir(directory)
+
+# 讀取每個 CSV 檔案，並將它們存儲在一個列表中
+dfs = [pd.read_csv(f"{directory}\\{filename}") for filename in filenames if filename.endswith('.csv')]
+
+# 檢查 dfs 是否為空
+if dfs:
+    # 合併所有的 DataFrame
+    df = pd.concat(dfs, ignore_index=True)
+
+    # 將合併後的 DataFrame 寫入到一個新的 CSV 檔案並存於此位置C:\Users\User\OneDrive\桌面\AI與土木應用\GitHub\cycu_ai2024_shing\20240430\M04A
+    df.to_csv(r"C:\Users\User\OneDrive\桌面\AI與土木應用\GitHub\cycu_ai2024_shing\20240430\M04A\20240429.csv", index=False)
+    print("所有 CSV 檔案已成功合併並存儲在 C:\\Users\\User\\OneDrive\\桌面\\AI與土木應用\\GitHub\\cycu_ai2024_shing\\20240430\\M04A\\HW04A.csv")     
 else:
     print("在指定的目錄下沒有找到任何 CSV 檔案。")

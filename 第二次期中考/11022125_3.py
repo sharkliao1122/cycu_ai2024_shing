a937bd3bc98a5c85de350776d6b7ac850d86b3ea
Shing1122 <160597889+Shing1122@users.noreply.github.com>
2024-05-31 16:47:33 +0800

    Update 11022125_3.py

--- 第二次期中考/11022125_3.py
+++ 第二次期中考/11022125_3.py
@@ -1,7 +1,21 @@
-import pandas as pd
+# 讀取 CSV 檔案 "C:\Users\User\OneDrive\M05A\M05A_整理\M05A_20240429.csv"
+# 轉換成 Dataframe，整理 Dataframe
+# 若資料中有 TimeInterval GantryFrom GantryTo 皆相同的資料合併成同一行，並保留 v31, v32, v41, v5 ，SpaceMeanSpeed 的第一個出現的值
 
+# M05A_0429_INFOR 整理
+# 只保留 "編號" "緯度(北緯)" "經度(東經)" 三個欄位，將 經度(東經) 改名為 "經度" 緯度(北緯) 改名為 "緯度"
+# 將 編號 欄位的值調整如下
+# 01F-000.5S 改為 01F0005S
+# 05F-R14.3N 改為 05FR143N 依此類推
+# 只保留 "編號" "緯度(北緯)" "經度(東經)" 三個欄位
+# 將 '經度(東經)' 改名為 '經度'，'緯度(北緯)' 改名為 '緯度'
 
-
+# M05A_0429 整理
+# 於 M05A_0429 新增 4 個欄位 "GantryFrom_緯度" "GantryFrom_經度" "GantryTo_緯度" "GantryTo_精度"，移除原本的 GantryFrom GantryTo 欄位
+# "GantryFrom_緯度" 的值應為 M05A_0429_INFOR 中 "編號" 等於 M05A_0429 中 "GantryFrom" 的 "緯度" 值
+# "GantryFrom_經度" 的值應為 M05A_0429_INFOR 中 "編號" 等於 M05A_0429 中 "GantryFrom" 的 "經度" 值
+# "GantryTo_緯度" 的值應為 M05A_0429_INFOR 中 "編號" 等於 M05A_0429 中 "GantryTo" 的 "緯度" 值
+# "GantryTo_經度" 的值應為 M05A_0429_INFOR 中 "編號" 等於 M05A_0429 中 "GantryTo" 的 "經度" 值
 
 import pandas as pd
 

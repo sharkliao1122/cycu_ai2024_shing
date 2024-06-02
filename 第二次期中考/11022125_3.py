c5e8e33a6de2971103f4350fe23ffef841b85cf2
Shing1122 <160597889+Shing1122@users.noreply.github.com>
2024-06-02 22:58:59 +0800

    20240524

--- 第二次期中考/11022125_3.py
+++ 第二次期中考/11022125_3.py
@@ -18,6 +18,11 @@
 # "GantryTo_經度" 的值應為 M05A_0429_INFOR 中 "編號" 等於 M05A_0429 中 "GantryTo" 的 "經度" 值
 
 import pandas as pd
+import webbrowser
+import folium
+from folium.plugins import TimestampedGeoJson
+import geopandas as gpd
+from shapely.geometry import LineString
 
 # 讀取 CSV 檔案並轉換成 Dataframe
 M05A_0429 = pd.read_csv(r'C:\Users\User\OneDrive\M05A\M05A_整理\M05A_20240429.csv')
@@ -32,15 +37,85 @@ M05A_0429_INFOR['編號'] = M05A_0429_INFOR['編號'].str.replace('-', '').str.r
 
 # 新增欄位並移除原本的 GantryFrom 和 GantryTo 欄位
 for direction in ['From', 'To']:
-    M05A_0429 = M05A_0429.merge(M05A_0429_INFOR, left_on=f'Gantry{direction}', right_on='編號', how='left').rename(columns={'緯度': f'Gantry{direction}_緯度', '經度': f'Gantry{direction}_經度'}).drop(['編號', f'Gantry{direction}'], axis=1)
+    M05A_0429 = M05A_0429.merge(M05A_0429_INFOR, left_on=f'Gantry{direction}', right_on='編號', how='left').rename(columns={'緯度': f'Gantry{direction}_緯度', '經度': f'Gantry{direction}_經度'}).drop(['編號'], axis=1)
 
 # 顯示數據框
 print(M05A_0429)
 
 
+# 用 folium  創建台灣地圖
+# 圖的內容為 2024 04 29 的 交通量 與車速 隨時間與高速公路里程的變化
+# 要有可拖動的時間軸 (M05_0429 內的 TimeInterval 當做時間 )
+# 需要用兩條線段連結 一條為北向另一條為南向 條件如下
+# GantryFrom_緯度 GantryFrom_經度  GantryTo_緯度  GantryTo_經度 來畫線段 
+# GantryFrom 最後一個字元分辨南北向 N:北向 S:南向
+# 線段顏色依據 SpaceMeanSpeed 來決定(白色:為無資料 紫色:0-20 紅色:20-40 橘色:40-60 黃色:60-80 綠色:大於80)
+# 點須能夠分行顯示 時間( 根據 TimeInterval) 車速( 根據 SpaceMeanSpeed) 車流量 (根據 v31 ) 的資料
 
 
+# 定義一個函數來根據 SpaceMeanSpeed 決定顏色
+def get_color(speed):
+    if pd.isna(speed):
+        return 'white'
+    elif speed <= 20:
+        return 'purple'
+    elif speed <= 40:
+        return 'red'
+    elif speed <= 60:
+        return 'orange'
+    elif speed <= 80:
+        return 'yellow'
+    else:
+        return 'green'
 
+# 讀取數據
+df = M05A_0429
+
+
+# 創建 GeoDataFrame
+gdf = gpd.GeoDataFrame(df, geometry=[LineString([(row['GantryFrom_經度'], row['GantryFrom_緯度']), (row['GantryTo_經度'], row['GantryTo_緯度'])]) for idx, row in df.iterrows()])
+
+# 創建 GeoJSON 物件
+def create_feature(row):
+    return {
+        'type': 'Feature',
+        'geometry': row['geometry'].__geo_interface__,
+        'properties': {
+            'time': row['TimeInterval'],
+            'style': {'color': get_color(row['SpaceMeanSpeed'])},
+            'icon': 'circle',
+            'iconstyle': {
+                'fillColor': get_color(row['SpaceMeanSpeed']),
+                'fillOpacity': 0.8,
+                'stroke': 'true',
+                'radius': 7
+            },
+            'popup': f"TimeInterval: {row['TimeInterval']}<br>SpaceMeanSpeed: {row['SpaceMeanSpeed']}<br>V31: {row['v31']}"
+        }
+    }
+
+features = gdf.apply(create_feature, axis=1).tolist()
+geo_json = {'type': 'FeatureCollection', 'features': features}
+
+# 創建地圖
+m = folium.Map(location=[23.5, 121], zoom_start=7)
+
+# 創建 TimestampedGeoJson 物件並加入到地圖
+TimestampedGeoJson(
+    geo_json,
+    period='PT1H',
+    add_last_point=True,
+    auto_play=False,
+    loop=False,
+    max_speed=1,
+    loop_button=True,
+    date_options='YYYY/MM/DD HH:mm',
+    time_slider_drag_update=True
+).add_to(m)
+
+# 保存並打開地圖
+m.save('map.html')
+webbrowser.open('file://' + os.path.realpath('map.html'))
 
 
 

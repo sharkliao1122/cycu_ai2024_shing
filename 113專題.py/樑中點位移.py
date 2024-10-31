a0f6fb1acd99b0f1b81a42a2972c1b276552683a
Shing1122 <160597889+Shing1122@users.noreply.github.com>
2024-10-31 16:35:27 +0800

    20241031

--- 113專題.py/樑中點位移.py
+++ 113專題.py/樑中點位移.py
@@ -24,19 +24,16 @@ df_三向_位移 = df_三向_位移.dropna(subset=['StepNum'])
 df_雙向_加速度 = df_雙向_加速度.dropna(subset=['StepNum'])
 df_三向_加速度 = df_三向_加速度.dropna(subset=['StepNum'])
 
-# 繪製6張圖表 分別 為 df_雙向  和 df_三向 中 'U1','U2','U3' 對 'StepNum'  的 折線圖
-# 將同張圖 要有 時間與位移 以及 時間與加速度
-# X 軸為  StepNum  Y 軸為 U1(M)
-# 雙向
-plt.figure(figsize=(15, 15))
-plt.subplot(3, 2, 1)
-
-plt.plot(df_雙向_位移['StepNum'], pd.to_numeric(df_雙向_位移['U1'], errors='coerce'), label='U1(M)')
-plt.plot(df_雙向_加速度['StepNum'], pd.to_numeric(df_雙向_加速度['U1'], errors='coerce'), label='U1(M/s^2)')
-plt.title('U1(M) AND U1(M/s^2) vs StepNum')
-plt.xlabel('StepNum')
-plt.ylabel('U1(M)')
-plt.legend()
-
-# Show all plots at the end
-plt.show()
+# 繪製12 張圖表 分別 為 
+# df_雙向_位移    U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 共三張圖 'U1','U2','U3' 單位為 M
+# df_三向_位移    U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 共三張圖 'U1','U2','U3' 單位為 M
+# df_雙向_加速度  U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 共三張圖 'U1','U2','U3' 單位為 M/S^2
+# df_三向_加速度  U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 共三張圖 'U1','U2','U3' 單位為 M/S^2
+# X 軸為  StepNum 
+# 將圖片存於 輸入之 EXCEL 檔案相同資料夾，並依照  EXCEL 名稱將圖片命名 
+# 命名規則 
+# 1.若為 df_雙向_位移  中  U1對StepNum  ,U2對StepNum , U3對StepNum 命名為  EYUL 水平雙向主樑中點位移U1  EYUL 水平雙向主樑中點位移U2 EYUL 水平雙向主樑中點位移U3
+# 2.df_三向_位移    U1對StepNum  ,U2對StepNum , U3對StepNum 同理 但將檔名中的 "雙向" 改為 "三向" 
+# 3.若為 df_雙向_加速度 中 U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 命名為 EYUL 水平雙向主樑中點加速度U1 , EYUL 水平雙向主樑中點加速度U2
+# 4.df_三向_加速度  U1對StepNum  ,U2對StepNum , U3對StepNum  的 折線圖 同理 將檔名中的 "雙向" 改為 "三向"
+# 5.以上檔案皆須跟據 輸入的 EXCEL 之檔名做更改

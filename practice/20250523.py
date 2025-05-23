c58a1a3d6c8eddef206d615e54290a857a02b7ed
Shing1122 <160597889+Shing1122@users.noreply.github.com>
2025-05-23 15:40:56 +0800

    1122

---
+++ practice/20250523.py
@@ -0,0 +1,9 @@
+number = input("please input an number :")
+sum = 0 
+number = int(number)
+for i in range(1,number+1):
+   sum += (10*i + 3)
+   print(sum)
+print(sum)
+c = 13 + 23 + 33 + 43 + 53 
+print(c)
